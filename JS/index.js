19de2ff2aaf01592605256d1d3383232126cf579
Matteo Aiello <matteo.aiello22@gmail.com>
2020-04-08 00:09:02 -0700

    major fixes

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,15 @@
 $(".intro-start").click(function () {
     $("html, body").animate({ scrollTop: $("#section2").offset().top }, 1000);
 });
+
+$(".intro-2").click(function () {
+    $("html, body").animate({ scrollTop: $("#section3").offset().top }, 1000);
+});
+
+$(".intro-3").click(function () {
+    $("html, body").animate({ scrollTop: $("#section4").offset().top }, 1000);
+});
+
+$(".intro-4").click(function () {
+    $("html, body").animate({ scrollTop: $("#section5").offset().top }, 1000);
+});
