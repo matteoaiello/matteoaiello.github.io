717708dd0b3206e4ee1bf2958f532d20c3595f79
Matteo Aiello <matteo.aiello22@gmail.com>
2020-04-20 22:42:53 -0700

    misc.

--- JS/index.js
+++ JS/index.js
@@ -17,3 +17,11 @@ $(".intro-3").click(function () {
 $(".intro-4").click(function () {
     $("html, body").animate({ scrollTop: $("#section5").offset().top }, 1000);
 });
+
+$(".intro-5").click(function () {
+    $("html, body").animate({ scrollTop: $("#section6").offset().top }, 1000);
+});
+
+$(".intro-6").click(function () {
+    $("html, body").animate({ scrollTop: $("#section7").offset().top }, 1000);
+});
