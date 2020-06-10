e8600b304b24975e971882f5151c25b8b5d23d09
Matteo Aiello <matteo.aiello22@gmail.com>
2020-06-09 22:07:14 -0700

    misc.

---
+++ JS/mobileindex.js
@@ -0,0 +1,28 @@
+$(".intro-start").click(function () {
+    $("html, body").animate({ scrollTop: $("#section2").offset().top }, 1000);
+});
+
+$(".intro-1").click(function () {
+    $("html, body").animate({ scrollTop: $("#section1").offset().top }, 1000);
+});
+
+$(".intro-2").click(function () {
+    $("html, body").animate({ scrollTop: $("#section3").offset().top }, 1000);
+});
+
+$(".intro-3").click(function () {
+    $("html, body").animate({ scrollTop: $("#section4").offset().top }, 1000);
+});
+
+$(".intro-4").click(function () {
+    $("html, body").animate({ scrollTop: $("#section5").offset().top }, 1000);
+});
+
+$(".intro-5").click(function () {
+    $("html, body").animate({ scrollTop: $("#section6").offset().top }, 1000);
+});
+
+$(".intro-6").click(function () {
+    $("html, body").animate({ scrollTop: $("#section7").offset().top }, 1000);
+});
+
