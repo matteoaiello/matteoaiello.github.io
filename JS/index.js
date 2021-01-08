5bba14eaf136077588d83b800bd184ea7d778137
Matteo Aiello <matteo.aiello22@gmail.com>
2021-01-07 17:39:04 -0700

    misc

--- JS/index.js
+++ JS/index.js
@@ -1,29 +1,29 @@
 $(".intro-start").click(function () {
-    $("html, body").animate({ scrollTop: $("#section2").offset().top }, 1200);
+    $("html, body").animate({ scrollTop: $("#section2").offset().top -50}, 1200);
 });
 
 $(".intro-1").click(function () {
-    $("html, body").animate({ scrollTop: $("#section1").offset().top }, 1200);
+    $("html, body").animate({ scrollTop: $("#section1").offset().top -50}, 1200);
 });
 
 $(".intro-2").click(function () {
-    $("html, body").animate({ scrollTop: $("#section3").offset().top }, 1200);
+    $("html, body").animate({ scrollTop: $("#section3").offset().top -50}, 1200);
 });
 
 $(".intro-3").click(function () {
-    $("html, body").animate({ scrollTop: $("#section4").offset().top }, 1200);
+    $("html, body").animate({ scrollTop: $("#section4").offset().top -50}, 1200);
 });
 
 $(".intro-4").click(function () {
-    $("html, body").animate({ scrollTop: $("#section5").offset().top }, 1200);
+    $("html, body").animate({ scrollTop: $("#section5").offset().top-50}, 1200);
 });
 
 $(".intro-5").click(function () {
-    $("html, body").animate({ scrollTop: $("#section6").offset().top }, 1200);
+    $("html, body").animate({ scrollTop: $("#section6").offset().top -50}, 1200);
 });
 
 $(".intro-6").click(function () {
-    $("html, body").animate({ scrollTop: $("#section7").offset().top }, 1200);
+    $("html, body").animate({ scrollTop: $("#section7").offset().top -50}, 1200);
 });
 
 /************************************************* PASCAL JS *************************************/
